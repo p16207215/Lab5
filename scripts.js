7fc91b18c26bec7bb751a41f07c59fd666629069
Priyansh shah <shahpriyansh74@gmail.com>
2018-11-12 11:18:21 +0000

    Lab 5 work

---
+++ scripts.js
@@ -0,0 +1,9 @@
+use-strict;
+
+const theform = document.getElementById("the-form");
+const thedate = document.querySelector(input[type="date"]);
+const getDate = document.querySelector(input[type="date"]);
+
+function showDate() {
+    let d = new Date(getDate.value);
+}
